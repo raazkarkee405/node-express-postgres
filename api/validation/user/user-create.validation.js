bc353c0cdf878faa5d7ef198ed8c705b9be9e1c3
Durgaraj <raazkarkee405@gmail.com>
2020-07-29 14:52:45 +0545

    create validation updated

--- api/validation/user/user-create.validation.js
+++ api/validation/user/user-create.validation.js
@@ -1,25 +1,22 @@
 const Validator = require('validator');
 const isEmpty = require('../isEmpty');
+const helper = require('../../utilities/helper');
 
 module.exports = function createUserValidation(data) {
     let errors = {};
     data.username = !isEmpty(data.username) ? data.username : "";
-   // data.phone = !isEmpty(data.phone) ? data.phone: "";
-  //  data.address = !isEmpty(data.address) ? data.address: "";
+   
     data.email = !isEmpty(data.email) ? data.email: "";
     data.password = !isEmpty(data.password) ? data.password: "";
 
     if (Validator.isEmpty(data.username)){
         errors.username = "username is required";
     }
-    // if (Validator.isEmpty(data.phone)){
-    //     errors.phone = "phone is required";
-    // }
-    // if (Validator.isEmpty(data.address)){
-    //     errors.address = "address is required";
-    // }
+
     if (Validator.isEmpty(data.email)){
         errors.email = "email is required";
+    }else if (!helper.validateEmail(data.email)){
+        errors.email = 'invalid email';
     }
 
     if(Validator.isEmpty(data.password)){
