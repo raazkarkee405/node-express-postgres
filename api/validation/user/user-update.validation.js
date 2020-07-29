ba8d846e1d36fd624a533763d93998f504404b86
Durgaraj <raazkarkee405@gmail.com>
2020-07-29 14:53:09 +0545

    update validation added

---
+++ api/validation/user/user-update.validation.js
@@ -0,0 +1,38 @@
+const isEmpty = require('../isEmpty');
+const   Validator = require('validator')
+
+module.exports = function validateUpateProfileParam(data){
+    let errors = {};
+    console.log(data);
+    if(data.hasOwnProperty('username')){
+        data.username = !isEmpty(data.username) ? data.username :"";
+        if(Validator.isEmpty(data.username)){
+            errors.username = "Name cannot be empty"
+        }
+    }
+    if(data.hasOwnProperty('phone')){
+        data.phone = !isEmpty(data.phone) ? data.phone :"";
+        if(Validator.isEmpty(data.phone)){
+            errors.phone = "Phone Number cannot be empty"
+        }
+    }
+    if(data.hasOwnProperty('address')){
+        data.address = !isEmpty(data.address) ? data.address:"";
+        if(Validator.isEmpty(data.address)){
+            errors.address = "Address cannot be empty"
+        }
+    }
+    
+    // if(data.hasOwnProperty('email')){
+    //     data.email = !isEmpty(data.email) ? data.email :"";
+    //     if(Validator.isEmpty(data.email)){
+    //         errors.email = "Email cannot be empty"
+    //     }
+    // }
+
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
