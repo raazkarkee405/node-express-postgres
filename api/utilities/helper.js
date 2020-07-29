e8fe899967f5ac73fcea3c7dad125563a3114757
Durgaraj <raazkarkee405@gmail.com>
2020-07-29 14:54:25 +0545

    helper added as utilities

---
+++ api/utilities/helper.js
@@ -0,0 +1,7 @@
+let validateEmail = (email) => {
+    var emailReg = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    return emailReg.test(String(email).toLowerCase());
+} 
+
+
+module.exports = {validateEmail}
